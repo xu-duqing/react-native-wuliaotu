010f62878e7c067b3aa1eef8a3dcb731d1a0a2c3
许度庆 <xu-duqing@gmail.com>
2015-11-02 21:56:36 +0800

    ADD 结构调整，增加详情页

--- WuLiaoTu/App/Views/Home/ListView/index.js
+++ WuLiaoTu/App/Views/Home/ListView/index.js
@@ -1,20 +1,65 @@
 /**
- * Create by Guang on 2015-10-21
+ * Create by Guang 2015-11-2
  */
 
 'usr strict'
 
-var React = require("react-native");
-var TuItem = require("./TuListItem");
-
+var React = require('react-native');
 var {
-	View,
-	StyleSheet,
+	Image,
 	Text,
 	ListView,
-} = React
+	View,
+	TouchableHighlight,
+} = React;
+
+var styles = require('./style');
 
 var REQUEST_URL = 'http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_pic_comments';
+
+var TuItem = React.createClass({
+
+	getInitialState : function(){
+		return{
+			loding : false,
+		};
+	},
+
+	_onPress:function(path){
+		this.props.onPress(path);
+	},
+
+	render : function(){
+
+		console.log(this.props.image);
+		return(
+			<TouchableHighlight onPress={() => this._onPress(this.props.image)}>
+				<View style = {styles.item}>
+					<Image 
+					style = {styles.image} 
+					source={{uri : this.props.image}}
+					onLoadStart = {() => {this.state.loding = true}}
+					onLoadEnd = {() => {this.state.loding = false}}>
+						{this.lodingView}
+					</Image>
+				</View>
+			</TouchableHighlight>
+			);
+	},
+
+	lodingView : function(){
+		if (this.state.loding) {
+			return(
+			<Text>
+				图片加载中....
+			</Text>
+			);
+		}
+		return null;
+	},
+
+
+});
 
 var TuListView = React.createClass({
 
@@ -54,7 +99,7 @@ var TuListView = React.createClass({
 	},
 	renderLodingView : function(){
 		return (
-				<View style = {styles.container}>
+				<View style = {styles.loding}>
 					<Text>
 						数据加载中，请稍后...
 					</Text>
@@ -63,8 +108,12 @@ var TuListView = React.createClass({
 	},
 	renderScoreboard : function(rowData){
 		return(
-			<TuItem image = {rowData.pics[0]} />
+			<TuItem onPress={this._onPress} image = {rowData.pics[0]} />
 			);
+	},
+
+	_onPress:function(path){
+		this.props.onPushDetail(path);
 	},
 
 	render : function(){
@@ -72,24 +121,8 @@ var TuListView = React.createClass({
 		if (!this.state.loaded) {
 			return this.renderLodingView();
 		};
-
 		return this.renderListView(this.state.dataSource);
 	}
-});
-
-var styles = StyleSheet.create({
-
-  	container :{
-		flex: 1,
-	    justifyContent: 'center',
-	    alignItems: 'center',
-   		backgroundColor: '#F5FCFF',
-  	},
-
-  	list : {
-
-  	},
-
 });
 
 module.exports = TuListView;
---
+++ WuLiaoTu/App/Views/Home/style.js
@@ -0,0 +1,14 @@
+/**
+ * Create by Guang 2015-11-2
+ */
+
+'usr strict'
+
+var React = require('react-native');
+
+module.exports = React.StyleSheet.create({
+
+	container:{
+		flex:1,
+	},
+});
